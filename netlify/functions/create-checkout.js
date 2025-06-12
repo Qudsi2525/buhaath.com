e0e2ebcc54c6b720bd098701b7e78e595129f55c
Qudsi2525 <Qudsi2525@hotmail.com>
2025-06-12 20:46:40 +0300

    Create create-checkout.js

---
+++ netlify/functions/create-checkout.js
@@ -0,0 +1,7 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+  const { email } =
